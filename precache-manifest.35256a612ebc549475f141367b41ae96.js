f5ac68ef7d0a7bfc550ab5a3ebaaf0dd45f772bb
Andriy Kyrylenko <z9877969@ukr.net>
2020-02-07 11:58:53 +0200

    Updates

---
+++ precache-manifest.35256a612ebc549475f141367b41ae96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ce31eda2830363916f8aa143384ddc",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "c7ea01e6d0b57b3a9ae9",
+    "url": "/star-wars/static/css/main.94cbba56.chunk.css"
+  },
+  {
+    "revision": "d507abe3442311980958",
+    "url": "/star-wars/static/js/2.579422a4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/star-wars/static/js/2.579422a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ea01e6d0b57b3a9ae9",
+    "url": "/star-wars/static/js/main.cb54a0b2.chunk.js"
+  },
+  {
+    "revision": "bb19ec81ea61bb80c6e1",
+    "url": "/star-wars/static/js/runtime-main.73f5171a.js"
+  }
+]);
